e495afb99d2fcff182a5739361a4944dc0ec33da
Johnny van Altvorst <johnnyva@live.com>
2017-08-05 10:18:30 +1200

    - Updated shopping-list-app - Added dropdown menu toggleOpen

--- shopping-list-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ shopping-list-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -8,10 +8,14 @@ import { Recipe } from "app/recipes/recipe.model";
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
+  //@Input() show: string = '';
   @Input() recipe: Recipe;
   constructor() { }
 
   ngOnInit() {
   }
 
+  //showDropdown(){
+  //  this.show = !this.show ? 'open' : '';
+  //}
 }
